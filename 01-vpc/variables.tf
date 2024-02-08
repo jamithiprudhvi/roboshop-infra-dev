962460a432092f8d4a88e85c4ee158a64365bcbe
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-02-09 04:14:50 +0530

    cidr

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_cidr" {
-  default = "10.1.0.0/16"
+  default = "10.0.0.0/16"
 }
 variable "common_tags" {
   default = {
@@ -22,15 +22,15 @@ variable "environment" {
 }
 
 variable "public_subnets_cidr" {
-  default = ["10.1.1.0/24", "10.1.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets_cidr" {
-  default = ["10.1.11.0/24", "10.1.12.0/24"]
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnets_cidr" {
-  default = ["10.1.21.0/24", "10.1.22.0/24"]
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
 variable "is_peering_required" {
